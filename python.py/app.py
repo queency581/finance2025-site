db3fc7d8565b2a4c4a76ff6ed59530be2393e5f7
queency581 <estellastephens20@gmail.com>
2025-08-10 19:40:04 +0300

    added succesfully

---
+++ python.py/app.py
@@ -0,0 +1,4 @@
+CLIENT_ID = ABNGFh2HBEBl8bO5d3ew57UoblHKSTibo7Yubzjdi3NCjvWSvP
+CLIENT_SECRET = Ao7jhJV4W1kzzPph22ndrhHR04a8EdYVrBd6FkmW
+REDIRECT_URI = https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl
+ENVIRONMENT = production
